cef9fb294c03221ffa6dc614187c21d88f41859b
Nelaturi <nelaturi@spinor.local>
2016-01-14 22:12:04 -0800

    Updated segmentation - cleaner structure

--- assembler/code/pybgs/mogbgs.py
+++ assembler/code/pybgs/mogbgs.py
@@ -7,6 +7,7 @@ Created on Tue Jan 12 17:25:19 2016
 """
 from numpy import *
 
+#import helpers
 # Implementation of 'Understanding Background Mixture Models for Foreground
 # Segmentation' by P.W. Power and J.A. Schoonees. This paper describes how
 # to implement 'Adaptive background mixture models for real-time tracking' 
@@ -53,32 +54,30 @@ def update_priors(X_t, match_t, t, weights_t, means_t, sigmas_t):
         sigmas_t[k] = sqrt((1-rho_kt)* pow(sigmas_t[k],2) + rho_kt * pow(X_t-means_t[k],2))   
         
 def segment(weights_t, sigmas_t):
-    # rank the states in each distribution 
-    mask = zeros_like(weights_t)
-    ranks = array([weight/sigma for (weight,sigma) in zip(weights_t,sigmas_t)])  
-    print 'weighted peaks =' +repr(ranks)
+    # rank the states in each distribution  
+    ranks = array([weight/sigma for (weight,sigma) in zip(weights_t,sigmas_t)])   
     temp = ranks.argsort() 
-    ranks[temp] = arange(len(ranks)) # rank positions measured by weight/sigma   
-    print 'ranked weighted peaks = ' +repr(ranks)
-    for i,j in enumerate(ranks.astype(int)): 
-        mask[j] = weights_t[i] 
-    print 'weights =' +repr(weights_t)
-    print 'Cumulative sums = ' +repr(cumsum(mask))
-    B = list(cumsum(mask) > 0.7).index(True) # see Equation 5
-    bg = (ranks <= B)
-    return bg
+    ranks[temp] = arange(len(ranks)) # rank positions measured by weight/sigma    
+    sorted_weights= array(sorted(zip(weights_t, ranks,range(len(weights_t))),key=lambda x:(-x[1],x[0])))
+    cumulative_weights = cumsum(sorted_weights[:,0]) 
+    bg_cutoff = list(cumulative_weights > 0.8).index(True)
+    bg = sorted_weights[:bg_cutoff+1,:]
+    fg = sorted_weights[bg_cutoff+1:,:]  
+    return bg,fg
         
 def main():
     # main function 
-    means= [80,100,200]
-    sigmas = [20,5,10] 
-    weights=[0.2,0.2,0.6]
-    #f = mog(x,weights, means,sigmas) 
-    #show(x,f)
+    means= [30, 64,128,192,220]
+    sigmas = [10,20,10,10,5] 
+    weights=[0.1, 0.1,0.2,0.4,0.2]
+    #x = linspace(0,256,300)
+    #f = helpers.mog(x,weights, means,sigmas) 
+    #helpers.show(x,f)
     # create current state for one of the components 
     X_t = list(array([100,101,101,100,100,102,99,97,101,150,151,150,100,100])+5)
     #print X_t
     
+    assert (sum(weights) == 1.0)
     t = 1
     for x_t in X_t:  
         t+=1
@@ -87,8 +86,11 @@ def main():
             # no matches found, classify X_t as foreground
             print 'no matches'
         else: 
-            print x_t 
-            print segment(weights, sigmas)   
+            #print x_t 
+            bg, fg = segment(weights, sigmas)   
+            bg_ids = bg[:,2]
+            fg_ids = fg[:,2]
+            print bg_ids, fg_ids 
             update_priors(x_t,matches,t,weights,means,sigmas)  
           
 
